dea72e8e96ca4b5af02d3a52940bbd549a8f4e92
Archanciel <jp.schnyder@gmail.com>
2018-12-30 20:51:28 +0100

    Drawing descent steps ok

--- basicGradientDescentAlgo.py
+++ basicGradientDescentAlgo.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Source URL: https://firsttimeprogrammer.blogspot.com/2014/09/gradient-descent-algorithm.html?m=1
+X_RANGE = 10
 x = Symbol('x')
 
 # Function
@@ -17,19 +18,19 @@ ypp = yprime.diff(x)
 # print(space)
 # print([N(y.subs(x, value)) for value in space])
 
+theta = X_RANGE
 
-def plotFun():
-    space = np.linspace(-5,5,100)
+def plotFun(x_range):
+    space = np.linspace(-x_range,x_range,100)
     data = np.array([N(y.subs(x,value)) for value in space])
     plt.plot(space, data)
     plt.show()
 
-theta = 81
 theta2 = 0
-alpha = .01
+alpha = .1
 iterations = 0
 check = 0
-precision = 1/1000000
+precision = 1/10000
 plot = True
 iterationsMax = 10000
 maxDivergence = 50
@@ -53,7 +54,7 @@ while True:
         if check > maxDivergence:
             print("Too much iterations (%s), the value of theta is diverging"%maxDivergence)
             print("Please choose a smaller alpha and, or check that the function is indeed convex")
-            plot = False
+#            plot = False
             break
           
     # If the value of theta changes less that a certain
@@ -62,10 +63,13 @@ while True:
     if abs(theta - theta2) < precision:
         break
     
+    print('theta: ', theta, '\ttheta2: ', theta2)
+    if plot:
+        plt.plot(theta, N(y.subs(x, theta)).evalf(), marker='o', color='r')
     theta = theta2
 
-
 if plot:
-    print("Number of iterations:",iterations,"value of theta:",theta2,sep=" ")
     plt.plot(theta,N(y.subs(x,theta)).evalf(),marker='o',color='r')
-    plotFun()
+    plotFun(X_RANGE)
+
+print("Number of iterations:",iterations,"value of theta:",theta2,sep=" ")
