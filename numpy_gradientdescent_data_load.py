4975240ae9f673120abf28d8be539d5df6fba9e3
Archanciel <jp.schnyder@gmail.com>
2019-01-18 16:59:08 +0100

    add chart plot

--- numpy_gradientdescent_data_load.py
+++ numpy_gradientdescent_data_load.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # loading data
 data = np.genfromtxt("simpledata.csv", delimiter=",")
@@ -16,15 +17,15 @@ print('\nX after adding col of ones')
 print(X)
 
 # extracting Y from data
-Y = data[:,-1:]
+y = data[:, -1:]
 print('\nY extracted from data')
-print(Y)
-Y = Y.flatten() # algo not working if not flattening !
-print('\nY flattened')
-print(Y)
+print(y)
+y = y.flatten() # algo not working if not flattening !
+print('\ny flattened')
+print(y)
 
 # starting theta values
-W = np.zeros(2)
+W = np.zeros(X.shape[1])
 
 print('\nW')
 print(W)
@@ -37,8 +38,10 @@ alpha = 0.01
 N = np.shape(X)[0]
 
 for i in range(epoch):
-    XdotW = np.dot(X, W)
-    XdotWMinusY = XdotW - Y
+    XdotW = np.dot(X, W) # The way that numpy is programmed means that a 1D array, shape=(n,),
+                         # is treated as either a column or a row vector based on the position
+    # in a dot product.
+    XdotWMinusY = XdotW - y
     G = np.dot(XdotWMinusY, X) / N
     if i < 5:
         print('iter ', i + 1)
@@ -70,3 +73,35 @@ for i in range(epoch):
         break
 
     W = newW
+
+#plt.scatter(X[:, 1].reshape(-1, 1), y, s = 500)
+
+plt.scatter(X[:, 1], y, s=50) # removed call to reshape()
+
+# axes = plt.gca()
+# x_vals = np.array(axes.get_xlim())
+# print('\nx_vals')
+# print(x_vals)
+# y_vals = W[0] + W[1] * x_vals # the line equation
+# print('\ny_vals')
+# print(y_vals)
+# plt.plot(x_vals, y_vals, '-')
+
+axes = plt.gca()
+xVals = np.array(axes.get_xlim())
+print('\nxVals')
+print(xVals)
+
+print('\nW')
+print(W)
+yVals = W[0] + W[1] * xVals # element wise addition and multiplication
+
+print('\nW[1] * xVals')
+print(W[1] * xVals)
+
+print('\nyVals')
+print(yVals)
+plt.plot(xVals, yVals)
+
+
+plt.show()
--- numpy_gradientdescent_no_data_load.py
+++ numpy_gradientdescent_no_data_load.py
@@ -1,15 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # data
 X = np.array([[1, 2], [1, 3], [1, 7]])
-Y = np.array([2, 5, 6])
+y = np.array([2, 5, 6])
 
 # theta values
-W = np.zeros(2)
-#W = np.ones(2)
+W = np.zeros(X.shape[1])
+#W = np.ones(X.shape[1])
 
-print('X, Y, W')
-print(X, Y, W)
+print('X, y, W')
+print(X, y, W)
 
 # now coding gradient descent
 epoch = 5000
@@ -17,12 +18,10 @@ precision = 0.0000001
 alpha = 0.01
 N = np.shape(X)[0]
 
-#points = np.genfromtxt("simpledata.csv", delimiter=",")
-#print(points)
-
 for i in range(epoch):
-    XdotW = np.dot(X, W)
-    XdotWMinusY = XdotW - Y
+    XdotW = np.dot(X, W) # The way that numpy is programmed means that a 1D array, shape=(n,),
+                         # is treated as either a column or a row vector based on the position
+    XdotWMinusY = XdotW - y
     G = np.dot(XdotWMinusY, X) / N
     if i < 5:
         print(i + 1,' ',G)
@@ -51,3 +50,21 @@ for i in range(epoch):
         break
 
     W = newW
+
+plt.scatter(X[:,1], y, s=50)
+axes = plt.gca()
+xVals = np.array(axes.get_xlim())
+print('\nxVals')
+print(xVals)
+
+print('\nW')
+print(W)
+yVals = W[0] + W[1] * xVals # element wise addition and multiplication
+
+print('\nW[1] * xVals')
+print(W[1] * xVals)
+
+print('\nyVals')
+print(yVals)
+plt.plot(xVals, yVals)
+plt.show()
